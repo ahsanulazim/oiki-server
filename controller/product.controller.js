445fa42ddb9035d55849aff216947a2f47a2e000
Ahsanul Azim Plabon <ahsanulazim6@gmail.com>
2026-05-20 05:29:03 +0600

    product upload api created 2

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -90,3 +90,18 @@ export const getAllProducts = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const getProductBySlug = async (req, res) => {
+  const { slug } = req.params;
+
+  try {
+    const product = await productCollection.findOne({ slug });
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    console.error("Error fetching product:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
--- router/product.route.js
+++ router/product.route.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   createProduct,
   getAllProducts,
+  getProductBySlug,
 } from "../controller/product.controller.js";
 
 const router = express.Router();
 
 router.post("/createProduct", createProduct);
 router.get("/getAllProducts", getAllProducts);
+router.get("/getProductBySlug/:slug", getProductBySlug);
 
 export default router;
