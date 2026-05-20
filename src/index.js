dbb84fd42817839f91860dc275456804e5221e4a
Ahsanul Azim Plabon <ahsanulazim6@gmail.com>
2026-05-21 00:26:28 +0600

    product upload api created 3

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://oiki-frontend.vercel.app"],
     credentials: true,
   }),
 );
